0dfa9852c3faf30fd73569ae74536cfc5c7e72cd
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-08-24 00:20:45 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import {Link,Routes,Route,BrowserRouter as Router} from 'react-router-dom'
-import UserApp from './UserApp/UserApp'
+import ProductOne from './ProductApp/ProductOne'
+import ProductTwo from './ProductApp/ProductTwo'
 const App = () => {
   return (
     <div>
         <Router>
         <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-            <Link to="#" className="navbar-brand">React User App</Link>
+            <Link to="#" className="navbar-brand">React Hooks</Link>
             <div className='ml-auto'>
                 <ul className='navbar-nav'>
-                    <li><Link className="nav-link" to="user">Users</Link></li>
-                   
+                    <li><Link className="nav-link" to="state">State</Link></li>
+                    <li><Link className="nav-link" to="classEx">Product - State-C</Link></li>
+                    <li><Link className="nav-link" to="funEx">Product - State-F</Link></li>
+                    <li><Link className="nav-link" to="digitalc">Digital</Link></li>
                 </ul>
             </div>
         </nav>
          <Routes>
-                <Route path="/user" element={<UserApp/>}/>
-              
+                <Route path="/classEx" element={<ProductOne/>}/>
+                <Route path="/funEx" element={<ProductTwo/>}/>
             </Routes>
         </Router>
     </div>
